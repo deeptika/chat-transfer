466720d5bc22bc9fbfabd797caa9a8d43d5b6ec3
dip_0421 <59311047+deeptika@users.noreply.github.com>
2023-04-20 18:17:33 -0400

    Version 2 - refactoring

--- Chat.java
+++ Chat.java
@@ -5,131 +5,110 @@ import java.nio.file.Files;
 import java.util.Objects;
 
 public class Chat {
-    private static final int serverPort = 4007;
+    public static void main(String[] args) throws IOException {
+        // creating local socket
+        ServerSocket localSocket = new ServerSocket(0);
 
-    public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(serverPort);
+        // starting server
+        System.out.println(localSocket.getLocalSocketAddress() + " started!\nListening on port " + localSocket.getLocalPort());
 
-        //connecting to the client
+        // start write thread
+        new WriteThread().start();
+
+        // connecting to remote port
+        Socket remoteSocket = localSocket.accept();
+        System.out.println("Obtained connection from " + remoteSocket.getRemoteSocketAddress() + " successfully!");
+
+        // start read thread
+        new ReadThread(remoteSocket).start();
+    }
+}
+
+class WriteThread extends Thread {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // to read socket input
+    private String message;    //message received from the remote port
+    private Socket remoteSocket; //socket that listens to other user
+    ObjectOutputStream outputStream = null; // stream to write to the socket
+    String currentDirectory = "./"; // current working directory of this thread
+
+    public void run() {
         try {
-            while (true) {
-                // starting server
-                System.out.println("Listening on port " + serverSocket.getLocalPort());
+            // connect to other user's port
+            System.out.println("What's the port of the user you'd like to connect to?");
+            remoteSocket = new Socket("localhost", Integer.parseInt(bufferedReader.readLine()));
+            System.out.println("Connected to " + remoteSocket.getRemoteSocketAddress() + " successfully!\n");
 
-                // connecting to client
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client " + clientSocket.getRemoteSocketAddress() + " is connected to the server!");
+            // reading message from output
+            while ((message = bufferedReader.readLine()) != null) {
+                outputStream = new ObjectOutputStream(remoteSocket.getOutputStream());
+                outputStream.writeObject(message);
 
-                // start write thread
-                new WriteThread(clientSocket).start();
+                // handling command "transfer <fileName>"
+                if (message.startsWith("transfer")) {
+                    String fileName = message.substring(9);
+                    boolean flag = false;
+                    File folder = new File(currentDirectory);
 
-                // start read thread
-                new ReadThread(clientSocket).start();
+                    for (File file : Objects.requireNonNull(folder.listFiles())) {
+                        if (file.getName().equals(fileName)) {
+                            System.out.println("Found file " + file.getName() + " in working directory, commencing transfer.");
+                            flag = true;
+                            byte[] content = Files.readAllBytes(file.toPath());
+                            outputStream.writeObject(content);
+                            System.out.println("File " + file.getName() + " transferred successfully!");
+                        }
+                    }
+                    if (!flag) {
+                        System.out.println("ERROR - File not found in this working directory!");
+                    }
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-            serverSocket.close();
-        }
-    }
-
-    private static class WriteThread extends Thread {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // to receive input from user
-        private String msgFromClient;    //message received from the client
-        private final Socket clientSocket;    //socket that listens to client
-        private Socket otherClientSocket; //socket that listens to other client
-        ObjectOutputStream outputStream = null; // stream to write to the socket
-        String currentDirectory = "./testFiles/"; // current working directory of this thread
-
-        public WriteThread(Socket clientSocket) {
-            this.clientSocket = clientSocket;
-        }
-
-        public void run() {
             try {
-                //initializing input and output streams
-                outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
                 outputStream.flush();
-
-                // connect to other user's port
-                System.out.println("What's the port of the user you'd like to connect to?");
-                int otherPort = Integer.parseInt(bufferedReader.readLine());
-                otherClientSocket = new Socket("localhost", otherPort);
-                System.out.println("Connected to: " + otherClientSocket.getRemoteSocketAddress());
-
-                // file transfer functionality
-                while ((msgFromClient = bufferedReader.readLine()) != null) {
-                    outputStream.writeObject(msgFromClient);
-                    outputStream.flush();
-
-                    String[] splitCommand = msgFromClient.split("\\s+");
-
-                    if (splitCommand.length == 2 && Objects.equals(splitCommand[0], "transfer")) {
-                        boolean flag = false;
-
-                        File folder = new File(currentDirectory);
-                        for (File file : Objects.requireNonNull(folder.listFiles())) {
-                            if (file.getName().equals(splitCommand[1])) {
-                                System.out.println("Found file " + file.getName() + " in working directory, commencing upload.");
-                                flag = true;
-
-                                byte[] content = Files.readAllBytes(file.toPath());
-                                outputStream.writeObject(content);
-                                System.out.println("File " + file.getName() + " transferred successfully!");
-                            }
-                        }
-                        if (!flag) {
-                            System.out.println("ERROR - File not found in this working directory!");
-                        }
-                    }
-                }
-            } catch (Exception e) {
+                outputStream.close();
+                bufferedReader.close();
+            } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                try {
-                    outputStream.flush();
-                    outputStream.close();
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
+}
 
-    private static class ReadThread extends Thread {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // to receive input from user
-        private String msgFromClient;    //message received from the client
-        private final Socket clientSocket;    //socket that listens to client
+class ReadThread extends Thread {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // to receive input from user
+    ObjectInputStream inputStream = null; // stream to read socket input
+    private String message;    // message received from the client
+    private final Socket socket;    // socket that listens to client
+    String currentDirectory = "./"; // current working directory of this thread
 
-        public ReadThread(Socket clientSocket) {
-            this.clientSocket = clientSocket;
-        }
+    public ReadThread(Socket socket) {
+        this.socket = socket;
+    }
 
-        public void run()   {
+    public void run()   {
+        try {
+            while ((message = bufferedReader.readLine()) != null) {
+                if (message.startsWith("transfer")) {
+                    inputStream = new ObjectInputStream(socket.getInputStream());
+                    String fileName = message.substring(9);
+                    File file = new File(currentDirectory + "new" + fileName);
+                    byte[] content = (byte[]) inputStream.readObject();
+                    Files.write(file.toPath(), content);
+                    System.out.println("File " + fileName + ": get is successful.");
+                } else {
+                    System.out.println(message);
+                }
+            }
+        } catch(Exception e)    {
+            e.printStackTrace();
+        }   finally {
             try {
-                while ((msgFromClient = bufferedReader.readLine()) != null) {
-                    String[] splitCommand = msgFromClient.split("\\s+");
-                    if (splitCommand.length == 2 && Objects.equals(splitCommand[0], "transfer")) {
-                        FileOutputStream fout = new FileOutputStream(splitCommand[1]);
-
-                        byte[] buffer = new byte[1024];
-                        int readPtr;
-                        while((readPtr = clientSocket.getInputStream().read(buffer)) != -1) {
-                            fout.write(buffer, 0, readPtr);
-                        }
-                        fout.close();
-                    }   else    {
-                        System.out.println(msgFromClient);
-                    }
-                }
-
-            } catch(Exception e)    {
+                bufferedReader.close();
+            } catch (IOException e) {
                 e.printStackTrace();
-            }   finally {
-                try {
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
